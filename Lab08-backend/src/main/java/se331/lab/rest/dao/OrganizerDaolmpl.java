b7fb10bcfca923475da9e607c3c59be3d05820d8
SupawichRatchataworrakun <supawich_r@elearning.cmu.ac.th>
2021-09-03 01:07:01 +0700

    7.1 / 7.2 (02)

---
+++ Lab08-backend/src/main/java/se331/lab/rest/dao/OrganizerDaolmpl.java
@@ -0,0 +1,118 @@
+package se331.lab.rest.dao;
+
+import org.aspectj.weaver.ast.Or;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import se331.lab.rest.entity.Organizer;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+@Profile("manual")
+public class OrganizerDaolmpl implements OrganizerDao {
+    List<Organizer> organizerList;
+
+    @PostConstruct
+    public void init() {
+        organizerList = new ArrayList<>();
+        organizerList.add(Organizer.builder()
+                .id(123L)
+                .category("animal welfare")
+                .title("Cat Adoption Day")
+                .description("Find your new feline friend at this event.")
+                .location("Meow Town")
+                .date("January 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Kat Laydee")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(456L)
+                .category("food")
+                .title("Community Gardening")
+                .description("Join us as we tend to the community edible plants.")
+                .location("Flora City")
+                .date("March 14, 2022")
+                .time("10:00")
+                .petAllowed(true)
+                .organizer("Fern Pollin")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(789L)
+                .category("sustainability")
+                .title("Beach Cleanup")
+                .description("Help pick up trash along the shore.")
+                .location("Playa Del Carmen")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Carey Wales")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(1001L)
+                .category("animal welfare")
+                .title("Dog Adoption Day")
+                .description("Find your new canine friend at this event.")
+                .location("Woof Town")
+                .date("August 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Dawg Dahd")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(1002L)
+                .category("food")
+                .title("Canned Food Drive")
+                .description("Bring your canned food to donate to those in need.")
+                .location("Tin City")
+                .date("September 14, 2022")
+                .time("3:00")
+                .petAllowed(true)
+                .organizer("Kahn Opiner")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(1003L)
+                .category("sustainability")
+                .title("Highway Cleanup")
+                .description("Help pick up trash along the highway.")
+                .location("Highway 50")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Brody Kill")
+                .build());
+    }
+
+    @Override
+    public Integer getOrganizerSize() {
+        return organizerList.size();
+    }
+
+    @Override
+    public Page<Organizer> getOrganizers(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? organizerList.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        return new PageImpl<Organizer>(organizerList.subList(firstIndex, firstIndex+pageSize), PageRequest.of(page,pageSize),organizerList.size());
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id) {
+        return organizerList.stream().filter(organizer -> organizer.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public Organizer save(Organizer organizer) {
+        organizer.setId(organizerList.get(organizerList.size()-1).getId()+1);
+        organizerList.add(organizer);
+        return organizer;
+    }
+}
